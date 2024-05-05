93f660757ada7c83f56603f9904aee4dcbccdb9d
naxsyambis <naxsyambis@gmail.com>
2024-05-05 02:05:05 +0100

    step 2 : memasukkan new node ke dalam list dengan syarat nol dan dimulai dari nomhs. tidak mengizinkan nomor yang duplikat. step 3 : jika list kosong maka buat new node sebagai start step 4 : buat start ke sebelumnya sebagai new node step 5 : new node sebagai start step 6: dan new node sebagai node pertama

--- 0163_Doublelinkedlist/0163_Doublelinkedlist.cpp
+++ 0163_Doublelinkedlist/0163_Doublelinkedlist.cpp
@@ -20,7 +20,23 @@ void addNode() {
     cout << "\nEnter the name of the student: ";
     cin >> newNode->name;  //Assign value to the data field of the new node
 
+    // Insert the new node in the list
+    if (START == NULL || newNode->noMhs <= START->noMhs)
+    { // step 2: insert the new node at the beginning
 
+        if (START != NULL && newNode->noMhs == START->noMhs)
+        {
+            cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+            return;
+        }
+        // if the list is empty, make the new node the START
+        newNode->next = START; // step 3: make the new node point to the frst node
+        if (START != NULL) {
+            START->prev = newNode; // step 4: make the first node point to the new node
+        }
+        newNode->prev = NULL; // step 5: make the new node point to NULL 
+        START = newNode; // step 6: make the new node the first node
+    }
 
 }
 
