4a1d8adee7aa58a6122ce94613dc71576765c440
naxsyambis <naxsyambis@gmail.com>
2024-05-05 02:56:23 +0100

    menambahkan syarat start harus nol, menampilkan list is empty step 1 : start dari node pertama

--- 0163_Doublelinkedlist/0163_Doublelinkedlist.cpp
+++ 0163_Doublelinkedlist/0163_Doublelinkedlist.cpp
@@ -93,6 +93,16 @@ void deleteNode()
     cout << "\nEnter the roll number of the student whose record is to be deleted: ";
     cin >> rollNo; // step 3: get the roll number number to be deleted
 
+    if (START == NULL)
+    {
+        cout << "List is empty" << endl;
+        return;
+    }
+
+    current = START; // step 1: start from the first node
+    previous = NULL;
+
+
 
 }
 
